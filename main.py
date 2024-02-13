d2bf7c15a3f04ef4438a3ca2cbc4be46f40a96fe
David Ohanjanian <87942705+nainajnahO@users.noreply.github.com>
2024-02-13 15:54:47 +0100

    Init commit.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import ucimlrepo
+import pandas
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from ucimlrepo import fetch_ucirepo
 
+# fetch dataset
+covertype = fetch_ucirepo(id=31)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# data (as pandas dataframes)
+X = covertype.data.features
+y = covertype.data.targets
 
+# metadata
+print(covertype.metadata)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Schöozz')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# variable information
+print(covertype.variables)
