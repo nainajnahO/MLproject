1c0d3ab84f2ff5f29f539c4837df77e558ab661c
David Ohanjanian <87942705+nainajnahO@users.noreply.github.com>
2024-02-13 16:22:46 +0100

    Init commit.

--- data_visualization.py
+++ data_visualization.py
@@ -1,3 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+# Import file
+df = pd.read_csv("train_dataset.csv")
+
+# Describe
+cover_type = df[' Forest Cover Type Classes']
+elevation = df['Elevation (meters)']
+
+
+print(cover_type)
+print(elevation)
